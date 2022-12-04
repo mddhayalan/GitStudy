d4d83293d6f584b4441f56825f6e7084c99dd491
Dheenadhayalan M <mddhayalan@gmail.com>
2022-12-04 19:30:43 +0530

    Adding new project

---
+++ WebApplication1Tests/BroadcastTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Tests
+{
+    [TestClass()]
+    public class BroadcastTests
+    {
+        [TestMethod()]
+        public void AdditionTest()
+        {
+            int x = 1;
+            int y = 2;
+
+            Assert.AreEqual(WebApplication1.Broadcast.Addition(x, y), x + y);
+        }
+
+        [TestMethod()]
+        public void BroadcastMessageTest()
+        {
+            string expected = "Hello World";
+            Assert.AreEqual(expected, Broadcast.BroadcastMessage());
+        }
+    }
+}
